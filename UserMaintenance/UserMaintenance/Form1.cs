df33fd9b7da56680b460716bffb89fac67473669
Károly Szabó <karoly.szabo@stud.uni-corvinus.hu>
2021-10-03 21:11:29 +0200

    4es

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,9 +12,21 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            
         }
+        void LoadData() 
+        {
+            Flats = context.Flat.ToList();
+        }
+
+
     }
 }
