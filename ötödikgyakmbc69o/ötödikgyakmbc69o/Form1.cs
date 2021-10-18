d3fa73b79c7a60d2343a2f6ea8108e4fb647e825
Károly Szabó <karoly.szabo@stud.uni-corvinus.hu>
2021-10-18 21:36:12 +0200

    5. Portfólió tárolása a memóriában

--- ötödikgyakmbc69o/ötödikgyakmbc69o/Form1.cs
+++ ötödikgyakmbc69o/ötödikgyakmbc69o/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ötödikgyakmbc69o.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,12 +15,29 @@ namespace ötödikgyakmbc69o
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio() 
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            //ez ugyanaz mint
+            //PortfolioItem p = new PortfolioItem();
+            //p.Index = "OTP";
+            //p.Volume = 10;
+            //Portfolio.Add(p);
+            dataGridView2.DataSource = Portfolio;
+
         }
     }
 }
