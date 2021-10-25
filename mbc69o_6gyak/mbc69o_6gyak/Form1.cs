cfe87b4a5d7f21fbe0d60675ef5a548cfbf3d190
Károly Szabó <karoly.szabo@stud.uni-corvinus.hu>
2021-10-25 11:37:13 +0200

    3. Webszolgáltatás hívása

--- mbc69o_6gyak/mbc69o_6gyak/Form1.cs
+++ mbc69o_6gyak/mbc69o_6gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mbc69o_6gyak.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,21 @@ namespace mbc69o_6gyak
         public Form1()
         {
             InitializeComponent();
+            AddParameters();
+        }
+
+        private void AddParameters()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
