99f02884cbd43bc54dbf2c5b69c19ef31226ea9b
Károly Szabó <karoly.szabo@stud.uni-corvinus.hu>
2021-10-25 12:00:05 +0200

    6. Adatok megjelenítése diagramon

--- mbc69o_6gyak/mbc69o_6gyak/Form1.cs
+++ mbc69o_6gyak/mbc69o_6gyak/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace mbc69o_6gyak
@@ -21,8 +22,9 @@ namespace mbc69o_6gyak
         {
             InitializeComponent();
             AddParameters();
-            dataGridView1.DataSource = "Rates";
+            dataGridView1.DataSource = Rates;
             XML();
+            Diagram();
 
         }
 
@@ -58,6 +60,25 @@ namespace mbc69o_6gyak
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+
+        private void Diagram()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
         }
     }
 }
