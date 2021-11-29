76fff3516867a848e89731319e73cdf13c6ed915
Károly Szabó <karoly.szabo@stud.uni-corvinus.hu>
2021-11-29 13:14:12 +0100

    Osztályok létrehozása

---
+++ gyak9__mbc69o/gyak9__mbc69o/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak9__mbc69o.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Deathprobability { get; set; }
+    }
+}
